56483f70e155cdb1908e9f526f1e10bf5d3bce29
Gabriel Acquaviva <gabrieldelara@hotmail.com>
2018-12-05 18:04:53 -0200

    Encerrando o loop

--- adivinhacao.py
+++ adivinhacao.py
@@ -4,13 +4,16 @@ print("********************************")
 
 numero_secreto = 42
 tentativas = 3
-rodada = 1
 
-while(rodada <= tentativas):
+for rodada in range(1, tentativas + 1):
     print("Tentativa {} de {}".format(rodada, tentativas))
-    chute_str = input("Digite o seu número: ")
+    chute_str = input("Digite o seu número entre 1 e 20: ")
     print("Você digitou", chute_str)
     chute = int(chute_str)
+
+    if(chute < 1 or chute > 20):
+        print("Você deve digitar um número entre 1 e 20!")
+        continue
 
     acertou = numero_secreto == chute
     maior = numero_secreto < chute
@@ -18,12 +21,11 @@ while(rodada <= tentativas):
 
     if(acertou):
         print("Acertou!")
+        break
     else:
         if(maior):
             print("Errou! O seu chute foi maior que o número secreto.")
         elif(menor):
             print("Errou! O seu chute foi menor que o número secreto.")
-
-    rodada = rodada + 1
 
 print("fim do jogo")
