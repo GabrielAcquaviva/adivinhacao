b1e192bee0dbfbda1d7777d5737bd8e56380c5e7
Gabriel Acquaviva <gabrieldelara@hotmail.com>
2018-12-10 16:39:12 -0200

    Inserindo nível e pontuação

--- adivinhacao.py
+++ adivinhacao.py
@@ -5,10 +5,26 @@ print("Bem vindo ao jogo de adivinhação")
 print("********************************")
 
 numero_secreto = random.randrange(1,21)
-tentativas = 3
+tentativas = 0
+pontos = 100
+
+print("Qual o nível de dificuldade?")
+print("(1) Fácil (2) Médio (3) Difícil")
+
+nivel = int(input("Escolha o nível: "))
+
+if(nivel == 1):
+    tentativas = 10
+
+elif(nivel == 2):
+    tentativas = 5
+
+else:
+    tentativas = 3
 
 for rodada in range(1, tentativas + 1):
     print("Tentativa {} de {}".format(rodada, tentativas))
+
     chute_str = input("Digite o seu número entre 1 e 20: ")
     print("Você digitou", chute_str)
     chute = int(chute_str)
@@ -22,12 +38,14 @@ for rodada in range(1, tentativas + 1):
     menor = numero_secreto > chute
 
     if(acertou):
-        print("Acertou!")
+        print("Acertou e fez {} pontos!".format(pontos))
         break
     else:
         if(maior):
             print("Errou! O seu chute foi maior que o número secreto.")
         elif(menor):
             print("Errou! O seu chute foi menor que o número secreto.")
+        pontos_perdidos = abs(numero_secreto - chute)
+        pontos = pontos - pontos_perdidos
 
 print("fim do jogo")
