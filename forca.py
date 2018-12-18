964c40b038324cdf31fd377619f0c19312ad8818
Gabriel Acquaviva <gabrieldelara@hotmail.com>
2018-12-18 18:29:25 -0200

    Manipulando as strings

--- forca.py
+++ forca.py
@@ -8,7 +8,17 @@ def jogar():
     enforcou = False
     acertou = False
 
-    while(not enforcou and not acertou)
+    while(not enforcou and not acertou):
+
+        chute = input("Qual letra? ")
+        chute = chute.strip()
+
+        index = 0
+        for letra in palavra_secreta:
+            if(chute.upper() == letra.upper()):
+                print("Encontrou a letra {} na posição {}".format(letra, index))
+            index = index  + 1
+
         print("jogando...")
 
     print("fim do jogo")
